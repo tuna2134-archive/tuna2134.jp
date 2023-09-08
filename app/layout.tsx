0476280b88240684d23404e5e27aa6bcca2156a9
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-08 11:53:07 +0000

    ok

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'tuna2134',
+  description: 'なんかある',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>
+        {children}
+        </body>
+    </html>
+  )
+}
