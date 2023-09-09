4d1808b922d2635ce378105894f05f4e790b64a5
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-09 10:32:11 +0900

    Update _components.tsx

--- app/_components.tsx
+++ app/_components.tsx
@@ -10,7 +10,7 @@ const links = [
 export const Header = () => {
   return (
     <header className="border-b">
-      <div className="h-20 w-full lg:max-w-4xl max-w-6xl px-4 mx-auto flex items-center">
+      <div className="h-20 w-full max-w-4xl lg:max-w-6xl px-4 mx-auto flex items-center">
         <div>
           <Link href="/" className="text-4xl font-bold">tuna2134</Link>
         </div>
