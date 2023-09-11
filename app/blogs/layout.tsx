f39d8e7de98f468dcbadce7867e1b0f952bdedc4
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-11 17:10:49 +0900

    Update layout.tsx

--- app/blogs/layout.tsx
+++ app/blogs/layout.tsx
@@ -8,7 +8,6 @@ const Layout = async ({
   children: React.ReactNode,
 }) => {
   const articles = await getArticles();
-  const article = articles[0];
   return (
     <div className="flex h-screen">
       <div className="w-1/4 border-r">
@@ -21,17 +20,7 @@ const Layout = async ({
           ))}
         </div>
       </div>
-      <div className="w-3/4">
-        /*
-        <div className="bg-black text-white p-auto">
-          <div className="h-22 flex items-center p-4">
-            <h2 className="text-4xl">{article.title}</h2>
-            <p className="ml-auto">{article.date.toDateString()}</p>
-          </div>
-        </div>
-        <div className="mx-6" dangerouslySetInnerHTML={{ __html: article.content }}></div>
-        */
-      </div>
+      <div className="w-3/4">{children}</div>
     </div>
   )
 };
